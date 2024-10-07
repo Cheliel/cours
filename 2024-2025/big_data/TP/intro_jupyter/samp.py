6e10de0b9c2f325d87aa7af86f614f9511795304
etienne <etienne.pernon@gmail.com>
2024-10-07 08:54:43 +0200

    MOD => big data

--- 2024-2025/big_data/TP/intro_jupyter/samp.py
+++ 2024-2025/big_data/TP/intro_jupyter/samp.py
@@ -1,14 +1,31 @@
-print("Donnez une valeur pour varInt :", end =" ")
-varInt = int(input())
 
-if varInt > 10 :
-    print("varInt est positif")
-elif varInt < 0 :
-    print("varInt est négatif")    
-elif varInt < 1 :
-    print("varInt est négatif")
-elif varInt < 2 :
-    print("varInt est négatif")
-else:
-    None
-    # print("varInt est égal à zéro")
+import copy
+a , b = [10], [20]
+c = copy.deepcopy(b)
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+__ = ["qsfd", 3532, [23, 43]]
+
+print(__[::-1])
+
+
+set = {
+     "eau",
+     "feu",
+     "terre",
+     "air"
+}
+
+set2 = {
+    "feu","eau"
+}
+
+#print(list(set - set2))
+
+
+
+
+
